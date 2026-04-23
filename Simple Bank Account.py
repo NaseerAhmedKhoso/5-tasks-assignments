d3d91ea902fb7ff7869a3eba35cc9803262263b3
NaseerAhmedKhoso <naseerahmedkhoso0@gmail.com>
2026-04-23 18:11:22 +0500

    Update Simple Bank Account.py

--- Simple Bank Account.py
+++ Simple Bank Account.py
@@ -6,15 +6,60 @@ class BankAccount:
         self.owner=owner
         self.balance=balance
 
-    def depositamount(self):
-        print(self.owner,"Deposited PKR ",self.balance ,"|","New balance : PKR",self.balance)
+    def deposit(self,amount):
+        self.balance+=amount
+        print(self.owner," You have Deposited PKR ",amount,"|","New balance : PKR",self.balance)
 
-acco1=BankAccount("Naseer Ahmed",300000)
-acco2=BankAccount("Ali",700000)
-acco3=BankAccount("Farooque",120000)
-acco4=BankAccount("Hussnain",50000)
+    def withdraw(self,amount):
+        self.balance-=amount
+        if amount<=self.balance:
+            print( self.owner,"You have withdrawed : PKR",amount," | Your new balance :PKR",self.balance)
+        else:
+            print("You have insufficint balance")
 
-acco1.depositamount()
-acco2.depositamount()
-acco3.depositamount()
-acco4.depositamount()
+    
+print(" Wellcome to ATM Machine")
+print("\n")
+print("1 : Deposit")
+
+print("2 : Withdraw")
+
+choice=int(input("Enter Your Choice :"))
+if choice==1:
+    print("1 : Naseer")
+    print("2 : Ali")
+    print("3 : Farooque")
+    print("4 : Hussnain")
+    choice=int(input("Enter your choice : "))
+    if choice==1:
+        Naseer=BankAccount("Naseer Ahmed",3000)
+        Naseer.deposit(int(input("Enter your amount : ")))
+    elif choice==2:
+        Ali=BankAccount("Ali",7000)
+        Ali.deposit(int(input("Enter your amount : ")))
+    elif choice==3:
+        Farooque=BankAccount("Farooque",1200)
+        Farooque.deposit(int(input("Enter your amount : ")))
+    elif choice==4:
+        Hussnain=BankAccount("Hussnain",500)
+        Hussnain.deposit(int(input("Enter your amount : ")))
+    
+    
+elif choice==2:
+    print("1 : Naseer")
+    print("2 : Ali")
+    print("3 : Farooque")
+    print("4 : Hussnain")
+    choice=int(input("Enter your choice"))
+    if choice==1:
+        Naseer=BankAccount("Naseer Ahmed",3000)
+        Naseer.withdraw(int(input("Enter your amount : ")))
+    elif choice==2:
+        Ali=BankAccount("Ali",7000)
+        Ali.withdraw(int(input("Enter your amount : ")))
+    elif choice==3:
+        Farooque=BankAccount("Farooque",1200)
+        Farooque.withdraw(int(input("Enter your amount : ")))
+    elif choice==4:
+        Hussnain=BankAccount("Hussnain",500)
+        Hussnain.withdraw(int(input("Enter your amount : ")))
